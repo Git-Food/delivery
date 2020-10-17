39955d8f66c5e1cfd8912e91c09efc45989d5b1c
Pedro Cantu <35717149+pedrocantu16@users.noreply.github.com>
2020-10-17 11:27:06 -0700

    Created Order class

--- src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
@@ -1,5 +1,28 @@
 package edu.northeastern.cs5500.delivery.model;
 
-public class Order {
-    
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.bson.types.ObjectId;
+import lombok.Data;
+
+@Data
+public class Order implements Model{
+    /**Constant valid charge amount for the order */
+    private static final Double VALID_AMOUNT = 0.0;
+    private ObjectId id;
+    private List<OrderItem> orderItems;
+    private OrderStatus orderStatus;
+    private Double taxAmount;
+    private Double totalPrice;
+    private ObjectId customerId;
+    private ObjectId businessId;
+
+    /**@return true if this Order is valid */
+    @JsonIgnore
+    public boolean isValid() {
+        return !this.orderItems.isEmpty() &&
+        this.taxAmount > VALID_AMOUNT &&
+        this.totalPrice > VALID_AMOUNT;
+    }
 }
---
+++ src/main/java/edu/northeastern/cs5500/delivery/model/OrderStatus.java
@@ -0,0 +1,11 @@
+package edu.northeastern.cs5500.delivery.model;
+
+
+/**
+ * Enum class for the status of an order.
+ */
+public enum OrderStatus {
+    Accepted, FoodIsBeingPrepared, CourierIsOnTheWay,
+    FoodIsArriving, Delivered
+
+}
