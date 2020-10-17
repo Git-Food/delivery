91c56ac8d26f7713f159bc107565d0f36b5158c4
Pedro Cantu <35717149+pedrocantu16@users.noreply.github.com>
2020-10-17 15:12:59 -0700

    Updated order class according to spoless java format

--- src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/Order.java
@@ -1,28 +1,32 @@
 package edu.northeastern.cs5500.delivery.model;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.bson.types.ObjectId;
+import java.util.List;
 import lombok.Data;
+import org.bson.types.ObjectId;
 
 @Data
-public class Order implements Model{
-    /**Constant valid charge amount for the order */
+public class Order implements Model {
+    /** Constant valid charge amount for the order */
     private static final double VALID_CHARGE = 0.0;
+    /** Constant to keep the delivery fee as % (percentage) */
+    private static final double DELIVERY_FEE = 0.03;
+
     private ObjectId id;
     private List<OrderItem> orderItems;
     private OrderStatus orderStatus;
+    private Double deliveryCharge;
     private Double taxAmount;
     private Double totalPrice;
     private ObjectId customerId;
     private ObjectId businessId;
 
-    /**@return true if this Order is valid */
+    /** @return true if this Order is valid */
     @JsonIgnore
     public boolean isValid() {
-        return !this.orderItems.isEmpty() &&
-        this.taxAmount > VALID_CHARGE &&
-        this.totalPrice > VALID_CHARGE;
+        return !this.orderItems.isEmpty()
+                && this.deliveryCharge >= DELIVERY_FEE
+                && this.taxAmount >= VALID_CHARGE
+                && this.totalPrice >= VALID_CHARGE;
     }
 }
--- src/main/java/edu/northeastern/cs5500/delivery/model/OrderStatus.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/OrderStatus.java
@@ -1,11 +1,14 @@
 package edu.northeastern.cs5500.delivery.model;
 
-
-/**
- * Enum class for the status of an order.
- */
+/** Enum class for the status of an order. */
 public enum OrderStatus {
-    Accepted, FoodIsBeingPrepared, CourierIsOnTheWay,
-    FoodIsArriving, Delivered
-
+    ACCEPTED,
+    FOOD_BEING_PREPARED,
+    COURIER_ON_WAY,
+    FOOD_ARRIVING,
+    DELIVERED,
+    REJECTED_BY_RESTAURANT,
+    NO_COURIER_AVAILABLE,
+    REFUNDED,
+    CANCELLED
 }
