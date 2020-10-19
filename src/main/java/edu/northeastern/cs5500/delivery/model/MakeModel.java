5dc95c0c2521bd3e0ffd7491ed078f8b48cfccf2
Pedro Cantu <35717149+pedrocantu16@users.noreply.github.com>
2020-10-19 15:30:21 -0700

    Refactored ModelModule for Vehicle and MakeModel

--- src/main/java/edu/northeastern/cs5500/delivery/model/MakeModel.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/MakeModel.java
@@ -1,19 +1,49 @@
 package edu.northeastern.cs5500.delivery.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Data;
-import org.bson.types.ObjectId;
+import java.util.Objects;
 
 /** Class represents the MakeModel of a Vehicle. */
-@Data
-public class MakeModel implements Model {
-    private ObjectId id;
+public class MakeModel {
     private String make;
     private String model;
+
+    public MakeModel(String make, String model) {
+        this.make = make;
+        this.model = model;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
 
     /** @return true if this MakeModel is valid */
     @JsonIgnore
     public boolean isValid() {
         return this.make != null && this.model != null;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        MakeModel that = (MakeModel) obj;
+        if (this.make.equals(that.make) && this.model.equals(that.model)) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.make, this.model);
+    }
+
+    @Override
+    public String toString() {
+        return "MakeModel = Make :" + this.make + " Model: " + this.model;
     }
 }
--- src/main/java/edu/northeastern/cs5500/delivery/model/ModelModule.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/ModelModule.java
@@ -19,4 +19,15 @@ public class ModelModule {
     public Class<Order> provideOrderClass() {
         return Order.class;
     }
+
+    @Provides
+    public MakeModel provideMakeModel(String name, String model) {
+        return new MakeModel(name, model);
+    }
+
+    @Provides
+    public Vehicle provideVehicle(
+            String color, String licensePlate, Integer year, MakeModel makeModel) {
+        return new Vehicle(color, licensePlate, year, makeModel);
+    }
 }
