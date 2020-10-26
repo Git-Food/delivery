ea13d4bdc72a9849999c742e45de4c11d15ff3a5
Stefan Hristov <shristov22@gmail.com>
2020-10-26 12:34:41 -0700

    added builder pattern to Name class

--- src/main/java/edu/northeastern/cs5500/delivery/model/Name.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/Name.java
@@ -1,9 +1,11 @@
 package edu.northeastern.cs5500.delivery.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class Name {
     private String firstName;
     private String lastName;
