7fa7505d464fd874b58f08230a27ce1343000dc2
timegao <50337470+timegao@users.noreply.github.com>
2020-11-02 12:53:15 -0800

    Changed List to Map in CustomerUser

--- src/main/java/edu/northeastern/cs5500/delivery/model/CustomerUser.java
+++ src/main/java/edu/northeastern/cs5500/delivery/model/CustomerUser.java
@@ -1,13 +1,13 @@
 package edu.northeastern.cs5500.delivery.model;
 
 import java.util.Date;
-import java.util.List;
+import java.util.Map;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class CustomerUser extends AbstractUser {
-    private List<Order> favoriteOrders;
+    private Map<ObjectId, Order> favoriteOrders;
     private Date birthday;
 }
