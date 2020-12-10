0727e897224fd7d8e8b49fd69d44de908638f22a
timegao <50337470+timegao@users.noreply.github.com>
2020-12-10 01:28:21 -0800

    Added additional menus and restaurant

--- src/main/java/edu/northeastern/cs5500/delivery/controller/RestaurantController.java
+++ src/main/java/edu/northeastern/cs5500/delivery/controller/RestaurantController.java
@@ -37,86 +37,161 @@ public class RestaurantController {
         log.info("RestaurantController > construct > adding default restaurants");
 
         // Build Menu
-        ObjectId menuItemObjectId1 = new ObjectId();
-        ObjectId menuItemObjectId2 = new ObjectId();
-        MenuItem defaultMenuItem1 = new MenuItem();
-        defaultMenuItem1.setId(menuItemObjectId1);
-        defaultMenuItem1.setName("Chicken1");
-        defaultMenuItem1.setDescription("chicken1 description");
-        defaultMenuItem1.setPrice(2);
-        defaultMenuItem1.setNote("Spicy sauce included");
-        defaultMenuItem1.setBusinessId(new ObjectId());
+        ObjectId businessObjectId1 = new ObjectId();
+        ObjectId businessObjectId2 = new ObjectId();
+        ObjectId businessObjectId3 = new ObjectId();
 
+        MenuItem defaultMenuItem1 = new MenuItem();
         MenuItem defaultMenuItem2 = new MenuItem();
-        defaultMenuItem2.setId(menuItemObjectId2);
-        defaultMenuItem2.setName("Beef1");
-        defaultMenuItem2.setDescription("beef1 description");
-        defaultMenuItem2.setPrice(3);
-        defaultMenuItem2.setNote("BBQ sauce included");
-        defaultMenuItem2.setBusinessId(new ObjectId());
+        MenuItem defaultMenuItem3 = new MenuItem();
+        MenuItem defaultMenuItem4 = new MenuItem();
+        MenuItem defaultMenuItem5 = new MenuItem();
+        MenuItem defaultMenuItem6 = new MenuItem();
+        MenuItem defaultMenuItem7 = new MenuItem();
+
+        defaultMenuItem1.setId(new ObjectId());
+        defaultMenuItem1.setName("Chicken burrito");
+        defaultMenuItem1.setDescription("Rice, beans, chicken");
+        defaultMenuItem1.setNote("Extra chicken");
+        defaultMenuItem1.setPrice(8);
+        defaultMenuItem1.setBusinessId(businessObjectId1);
+
+        defaultMenuItem2.setId(new ObjectId());
+        defaultMenuItem2.setName("Beef salad");
+        defaultMenuItem2.setDescription("Romaine salad, rice, beans, beef");
+        defaultMenuItem2.setPrice(9);
+        defaultMenuItem2.setNote("No source cream");
+        defaultMenuItem2.setBusinessId(businessObjectId1);
+
+        defaultMenuItem3.setId(new ObjectId());
+        defaultMenuItem3.setName("Kaiseki Gozen");
+        defaultMenuItem3.setDescription("7-8 course Kaiseki dishes");
+        defaultMenuItem3.setPrice(100);
+        defaultMenuItem3.setNote("");
+        defaultMenuItem3.setBusinessId(businessObjectId2);
+
+        defaultMenuItem4.setId(new ObjectId());
+        defaultMenuItem4.setName("The wa'z Don");
+        defaultMenuItem4.setDescription("The highest quality ikura, negitoro, and uni toro");
+        defaultMenuItem4.setPrice(50);
+        defaultMenuItem4.setNote("Available until Dec 31");
+        defaultMenuItem4.setBusinessId(businessObjectId2);
+
+        defaultMenuItem5.setId(new ObjectId());
+        defaultMenuItem5.setName("Party Temaki");
+        defaultMenuItem5.setDescription("Temaki plate for three");
+        defaultMenuItem5.setPrice(135);
+        defaultMenuItem5.setNote("4th of July special Temaki");
+        defaultMenuItem5.setBusinessId(businessObjectId2);
+
+        defaultMenuItem6.setId(new ObjectId());
+        defaultMenuItem6.setName("Shack Burger");
+        defaultMenuItem6.setDescription("Cheeseburger with lettuce and tomato");
+        defaultMenuItem6.setPrice(9);
+        defaultMenuItem6.setNote("Double");
+        defaultMenuItem6.setBusinessId(businessObjectId3);
+
+        defaultMenuItem7.setId(new ObjectId());
+        defaultMenuItem7.setName("Bacon cheese fries");
+        defaultMenuItem7.setDescription("Topped with cheese sauce and applewood-smoked bacon");
+        defaultMenuItem7.setPrice(5);
+        defaultMenuItem7.setNote("Niman Ranch");
+        defaultMenuItem7.setBusinessId(businessObjectId3);
 
         // Build menu items
         ArrayList<MenuItem> defaultMenuItems1 = new ArrayList<MenuItem>();
-        defaultMenuItems1.add(defaultMenuItem1);
         ArrayList<MenuItem> defaultMenuItems2 = new ArrayList<MenuItem>();
-        defaultMenuItems2.add(defaultMenuItem2);
+        ArrayList<MenuItem> defaultMenuItems3 = new ArrayList<MenuItem>();
+
+        defaultMenuItems1.add(defaultMenuItem1);
+        defaultMenuItems1.add(defaultMenuItem2);
+        defaultMenuItems2.add(defaultMenuItem3);
+        defaultMenuItems2.add(defaultMenuItem4);
+        defaultMenuItems2.add(defaultMenuItem5);
+        defaultMenuItems3.add(defaultMenuItem6);
+        defaultMenuItems3.add(defaultMenuItem7);
 
         final Menu defaultMenu1 = new Menu();
-        defaultMenu1.setName("menu name1");
-        defaultMenu1.setDescription("menu description1");
+        final Menu defaultMenu2 = new Menu();
+        final Menu defaultMenu3 = new Menu();
+
+        defaultMenu1.setName("Chipotle Menu");
+        defaultMenu1.setDescription("Fast food chain offering Mexican fare");
         defaultMenu1.setMenuItems(defaultMenuItems1);
         defaultMenu1.setId(new ObjectId());
 
-        final Menu defaultMenu2 = new Menu();
-        defaultMenu2.setName("menu name2");
-        defaultMenu2.setDescription("menu description2");
+        defaultMenu2.setName("Wa'z Menu");
+        defaultMenu2.setDescription("Artful kaiseki-style plates");
         defaultMenu2.setMenuItems(defaultMenuItems2);
         defaultMenu2.setId(new ObjectId());
 
+        defaultMenu3.setName("Shake Shack Menu");
+        defaultMenu3.setDescription("Hip, counter-server chain");
+        defaultMenu3.setMenuItems(defaultMenuItems3);
+        defaultMenu3.setId(new ObjectId());
+
         // Locations
-        PostalAddress address1 = new PostalAddress();
-        address1.setHouseNumber("410");
-        address1.setStreetAddress("Terry Avenue");
+        final PostalAddress address1 = new PostalAddress();
+        final PostalAddress address2 = new PostalAddress();
+        final PostalAddress address3 = new PostalAddress();
+
+        address1.setHouseNumber("212");
+        address1.setStreetAddress("Westlake Ave");
         address1.setCity("Seattle");
         address1.setState("WA");
         address1.setZipCode("98109");
         address1.setCountry("United States");
 
-        PostalAddress address2 = new PostalAddress();
-        address2.setHouseNumber("832");
-        address2.setStreetAddress("Dexter Avenue North");
+        address2.setHouseNumber("411");
+        address2.setStreetAddress("Cedar St");
         address2.setCity("Seattle");
         address2.setState("WA");
-        address2.setZipCode("98109");
+        address2.setZipCode("98121");
         address2.setCountry("United States");
+
+        address3.setHouseNumber("2115");
+        address3.setStreetAddress("Westlake Ave");
+        address3.setCity("Seattle");
+        address3.setState("WA");
+        address3.setZipCode("98121");
+        address3.setCountry("United States");
 
         // Build Restaurants
         final Restaurant defaultRestaurant1 = new Restaurant();
         final Restaurant defaultRestaurant2 = new Restaurant();
+        final Restaurant defaultRestaurant3 = new Restaurant();
 
-        defaultRestaurant1.setName("Best Food Restaurant");
-        // Grabs the menu id of the menu for the restaurant
+        defaultRestaurant1.setName("Chipotle");
         defaultRestaurant1.setMenuId(defaultMenu1.getId().toHexString());
-        defaultRestaurant1.setCuisineType("American");
+        defaultRestaurant1.setCuisineType("Mexican");
         defaultRestaurant1.setId(new ObjectId());
         defaultRestaurant1.setLocation(address1);
-        defaultRestaurant1.setStartTime(LocalTime.of(7, 00, 00));
-        defaultRestaurant1.setEndTime(LocalTime.of(7, 00, 00));
+        defaultRestaurant1.setStartTime(LocalTime.of(10, 45, 00));
+        defaultRestaurant1.setEndTime(LocalTime.of(22, 00, 00));
 
-        defaultRestaurant2.setName("Eatery");
-        // Grabs the menu id of the menu for the restaurant
+        defaultRestaurant2.setName("Wa'z Seattle");
         defaultRestaurant2.setMenuId(defaultMenu2.getId().toHexString());
-        defaultRestaurant2.setCuisineType("Chinese");
+        defaultRestaurant2.setCuisineType("Japanese");
         defaultRestaurant2.setId(new ObjectId());
         defaultRestaurant2.setLocation(address2);
-        defaultRestaurant2.setStartTime(LocalTime.of(8, 00, 00));
-        defaultRestaurant2.setEndTime(LocalTime.of(22, 00, 00));
+        defaultRestaurant2.setStartTime(LocalTime.of(18, 00, 00));
+        defaultRestaurant2.setEndTime(LocalTime.of(21, 00, 00));
+
+        defaultRestaurant3.setName("Shake Shack");
+        defaultRestaurant3.setMenuId(defaultMenu3.getId().toHexString());
+        defaultRestaurant3.setCuisineType("American");
+        defaultRestaurant3.setId(new ObjectId());
+        defaultRestaurant3.setLocation(address3);
+        defaultRestaurant3.setStartTime(LocalTime.of(11, 00, 00));
+        defaultRestaurant3.setEndTime(LocalTime.of(22, 00, 00));
 
         try {
             addRestaurant(defaultRestaurant1);
             addRestaurant(defaultRestaurant2);
+            addRestaurant(defaultRestaurant3);
             menuController.addMenu(defaultMenu1);
             menuController.addMenu(defaultMenu2);
+            menuController.addMenu(defaultMenu3);
         } catch (Exception e) {
             log.error(
                     "defaultRestaurant1Controller > construct > adding default restaurants > failure?");
